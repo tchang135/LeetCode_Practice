4576b54d9db85d91b081278107178a56b1042fa5
Timothy Chang <tchang135@gmail.com>
2023-08-17 13:30:10 -0400

    two sum practice

---
+++ Two Sum.rb
@@ -0,0 +1,33 @@
+# // Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+# // You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+# // You can return the answer in any order.
+
+# // Example 1:
+
+# // Input: nums = [2,7,11,15], target = 9
+# // Output: [0,1]
+# // Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+# // Example 2:
+
+# // Input: nums = [3,2,4], target = 6
+# // Output: [1,2]
+# // Example 3:
+
+# // Input: nums = [3,3], target = 6
+# // Output: [0,1]
+
+def two_sum(nums, target)
+    numsMap = {}
+
+    for i in 0...nums.length
+        complement = target - nums[i]
+        if numsMap.include?(complement)  # '.include?' for Ruby. Can also use '.key?' to check if it is a key in the numsMap
+            return [numsMap[complement], i]
+        end
+        numsMap[nums[i]] = i
+    end
+    return []
+end
+
