8ded6279745d47b7f97de1b350d24cc4f4db0cb7
Timothy Chang <tchang135@gmail.com>
2023-10-17 15:36:34 -0400

    lenght of longest substring rb

--- Medium Blind75/Longest Substring Without Repeating Characters.rb
+++ Medium Blind75/Longest Substring Without Repeating Characters.rb
@@ -21,6 +21,7 @@
 # Explanation: The answer is "wke", with the length of 3.
 # Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
 
+
 def length_of_longest_substring(s)
     letter_index = {}  
     max_length = 0
